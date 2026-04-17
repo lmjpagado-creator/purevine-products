024ef4c7185687ffc593a39e9c25a725baafe3f2
lmjpagado-creator <lmjpagado@gmail.com>
2026-04-18 03:21:25 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,23 +1,349 @@
 window.purevineProducts = [
+
+  // RIGID PACKAGING
   {
-    id: "produce-clamshell",
     category: "Rigid Packaging",
-    title: "Produce Clamshell Container",
-    image: "https://collection.cloudinary.com/dop5knltm/333016170a63d97ddd7fbe6568042ae5",
-    link: "/products#produce-clamshell"
+    subcategory: "General",
+    title: "Meat Trays",
+    image: ""
   },
   {
-    id: "berry-container",
     category: "Rigid Packaging",
-    title: "Berry Container",
-    image: "https://YOUR-CLOUDINARY-URL/berry.jpg",
-    link: "/products#berry-container"
+    subcategory: "General",
+    title: "PET Trays",
+    image: ""
   },
   {
-    id: "printed-film",
+    category: "Rigid Packaging",
+    subcategory: "General",
+    title: "PCR Trays",
+    image: ""
+  },
+  {
+    category: "Rigid Packaging",
+    subcategory: "General",
+    title: "PP Trays",
+    image: ""
+  },
+  {
+    category: "Rigid Packaging",
+    subcategory: "General",
+    title: "CPET Trays",
+    image: ""
+  },
+  {
+    category: "Rigid Packaging",
+    subcategory: "General",
+    title: "PP EVOH Trays",
+    image: ""
+  },
+  {
+    category: "Rigid Packaging",
+    subcategory: "General",
+    title: "PLA Trays",
+    image: ""
+  },
+  {
+    category: "Rigid Packaging",
+    subcategory: "General",
+    title: "IML Cups",
+    image: ""
+  },
+  {
+    category: "Rigid Packaging",
+    subcategory: "General",
+    title: "Palm Trays",
+    image: ""
+  },
+  {
+    category: "Rigid Packaging",
+    subcategory: "General",
+    title: "Bamboo Trays",
+    image: "https://res.cloudinary.com/dop5knltm/image/upload/q_auto/f_auto/v1776449121/8_1_zcdpez.jpg"
+  },
+
+  // PRODUCE & GREENHOUSE PACKAGING
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Produce",
+    title: "Greenhouse Packaging",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Produce",
+    title: "Field Pack Packaging",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Produce",
+    title: "Tomato Trays",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Produce",
+    title: "Berry Trays",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Produce",
+    title: "Lettuce Trays",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Film Seal",
+    title: "4 oz with film seal",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Film Seal",
+    title: "8 oz with film seal",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Containers",
+    title: "Top Seal Containers",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Clamshells",
+    title: "Vented Clamshells",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Clamshells",
+    title: "Non-Vented Clamshells",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Clamshells",
+    title: "Single Head Lettuce Clamshell",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Clamshells",
+    title: "Double Head Lettuce Clamshell",
+    image: ""
+  },
+  {
+    category: "Produce & Greenhouse Packaging",
+    subcategory: "Clamshells",
+    title: "Triple Head Lettuce Clamshell",
+    image: ""
+  },
+
+  // FLEXIBLE PACKAGING
+  {
     category: "Flexible Packaging",
-    title: "Printed Film",
-    image: "https://YOUR-CLOUDINARY-URL/film.jpg",
-    link: "/products#printed-film"
+    subcategory: "Films",
+    title: "Meat & Cheese Films",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Films",
+    title: "OTR Films",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Films",
+    title: "Printed Offset Films",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Films",
+    title: "Digital Printed Films",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Films",
+    title: "Printed Multi-Layer Films",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Bags & Pouches",
+    title: "Produce Bags",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Bags & Pouches",
+    title: "Roll Stock Bags",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Bags & Pouches",
+    title: "Bags & Pouches",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Bags & Pouches",
+    title: "Grape Bags with Handle",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Bags & Pouches",
+    title: "Produce Bags with Handle",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Lidding Film",
+    title: "Printed Lidding Film",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Lidding Film",
+    title: "Unprinted Lidding Film",
+    image: ""
+  },
+  {
+    category: "Flexible Packaging",
+    subcategory: "Films",
+    title: "Laser Perforated Film",
+    image: ""
+  },
+
+  // BAKERY PACKAGING
+  {
+    category: "Bakery Packaging",
+    subcategory: "Containers",
+    title: "Muffin Trays",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Containers",
+    title: "Cookie Containers",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Containers",
+    title: "Cake Trays",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Containers",
+    title: "Hinged Containers",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Containers",
+    title: "2-Piece Containers",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Accessories",
+    title: "Lidding Film",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Accessories",
+    title: "Paperboard Cookie Boxes with Window",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Accessories",
+    title: "Parchment Paper",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Accessories",
+    title: "Labels",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Accessories",
+    title: "Thermal Transfer Labels",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Accessories",
+    title: "Ribbon",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Packaging For",
+    title: "Cookies",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Packaging For",
+    title: "Brownies",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Packaging For",
+    title: "Muffins",
+    image: ""
+  },
+  {
+    category: "Bakery Packaging",
+    subcategory: "Packaging For",
+    title: "Cakes",
+    image: ""
+  },
+
+  // OTHERS
+  {
+    category: "Others",
+    subcategory: "Secondary Packaging",
+    title: "Film",
+    image: ""
+  },
+  {
+    category: "Others",
+    subcategory: "Secondary Packaging",
+    title: "Labels",
+    image: ""
+  },
+  {
+    category: "Others",
+    subcategory: "Secondary Packaging",
+    title: "Folded Carton Sleeves",
+    image: ""
+  },
+  {
+    category: "Others",
+    subcategory: "Secondary Packaging",
+    title: "V-Board",
+    image: ""
+  },
+  {
+    category: "Others",
+    subcategory: "Secondary Packaging",
+    title: "Straps",
+    image: ""
   }
+
 ];
